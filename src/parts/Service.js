98e0cdb6316e7944178e0161db7a25fae3530769
Suraj kumar <devilsuraj9641@gmail.com>
2025-01-28 00:05:43 +0530

    new

--- src/parts/Service.js
+++ src/parts/Service.js
@@ -9,7 +9,6 @@ import { Fade } from 'react-awesome-reveal';
 export default function Service({ data }) {
   return (
     <div className="bg-[#090C13] min-h-screen flex flex-col justify-center">
-      {/* Ensure full height */}
       <div className="container mx-auto pt-10 pb-28">
         <Fade direction="right" triggerOnce>
           <h1 className="text-5xl text-white text-center font-bold">Our Service</h1>
@@ -21,21 +20,29 @@ export default function Service({ data }) {
         </Fade>
 
         <div className="grid grid-rows-3 px-10 gap-8 sm:grid-cols-3 sm:grid-rows-1 sm:gap-6 xl:gap-16">
-          {
-            data.map((item) => (
-              // Use a unique identifier (e.g., `item.id`) as the key
-              <Fade direction={item.animation} delay={500 * data.indexOf(item)} key={item.id} triggerOnce>
-                <div>
-                  <div className="bg-white group rounded-2xl shadow-2xl border border-light-theme-purple transform transition duration-500 hover:scale-105">
-                    <img src={item.imageUrl} alt="Service" className="w-full rounded-t-2xl" />
-                    <h2 className="text-theme-blue text-center text-xl py-7 rounded-b-2xl">
-                      {item.title}
-                    </h2>
-                  </div>
+          {data.map((item) => (
+            <Fade
+              direction={item.animation}
+              delay={500 * data.indexOf(item)}
+              key={item.id}
+              triggerOnce
+            >
+              <div>
+                <div className="bg-white group rounded-2xl shadow-2xl border border-light-theme-purple 
+                  transform transition duration-500 hover:scale-105"
+                >
+                  <img
+                    src={item.imageUrl}
+                    alt={`Service - ${item.title}`}
+                    className="w-full rounded-t-2xl"
+                  />
+                  <h2 className="text-theme-blue text-center text-xl py-7 rounded-b-2xl">
+                    {item.title}
+                  </h2>
                 </div>
-              </Fade>
-            ))
-          }
+              </div>
+            </Fade>
+          ))}
         </div>
       </div>
     </div>
