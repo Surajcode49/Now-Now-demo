0a0acfd774af8f8fe697a42d79e5a6c24f9f9487
Suraj kumar <devilsuraj9641@gmail.com>
2025-01-27 23:39:47 +0530

    jai ram jii

--- src/parts/Service.js
+++ src/parts/Service.js
@@ -8,7 +8,8 @@ import { Fade } from 'react-awesome-reveal';
 
 export default function Service({ data }) {
   return (
-    <div className="bg-[#090C13] min-h-screen flex flex-col justify-center"> {/* Ensure full height */}
+    <div className="bg-[#090C13] min-h-screen flex flex-col justify-center">
+      {/* Ensure full height */}
       <div className="container mx-auto pt-10 pb-28">
         <Fade direction="right" triggerOnce>
           <h1 className="text-5xl text-white text-center font-bold">Our Service</h1>
@@ -21,8 +22,9 @@ export default function Service({ data }) {
 
         <div className="grid grid-rows-3 px-10 gap-8 sm:grid-cols-3 sm:grid-rows-1 sm:gap-6 xl:gap-16">
           {
-            data.map((item, index) => (
-              <Fade direction={item.animation} delay={500 * index} key={index} triggerOnce>
+            data.map((item) => (
+              // Use a unique identifier (e.g., `item.id`) as the key
+              <Fade direction={item.animation} delay={500 * data.indexOf(item)} key={item.id} triggerOnce>
                 <div>
                   <div className="bg-white group rounded-2xl shadow-2xl border border-light-theme-purple transform transition duration-500 hover:scale-105">
                     <img src={item.imageUrl} alt="Service" className="w-full rounded-t-2xl" />
